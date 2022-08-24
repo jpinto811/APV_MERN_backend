60aa26933353964fb768b795b448671942566e3b
jpinto811 <jpinto810@gmail.com>
2022-08-23 19:12:42 -0500

    first commit

---
+++ models/Paciente.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const pacienteSchema = mongoose.Schema({
+  nombre: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  propietario: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  fecha: {
+    type: Date,
+    required: true,
+    trim: true,
+    default: Date.now()
+  },
+  sintomas:{
+    type: String,
+    required: true,
+  },
+  veterinario: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Veterinario',
+  },
+}, {
+  timestamps: true,
+});
+
+const Paciente = mongoose.model("Paciente", pacienteSchema);
+
+export default Paciente;
